ae8434e8f48ae16cb7aa7f9fc2b52ebf43554e66
MAmir20 <mezghani.amir@gmail.com>
2024-06-22 12:58:38 +0100

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.layout')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Icons</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Icons</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Welcome to Laravel 8</h1>
+            </div>
+        </div>
+    </div>
+@endsection
